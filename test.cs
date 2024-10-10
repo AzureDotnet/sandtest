6762a66a75f99f49a091b1f018e8e0db3057d849
AzureDotnet <sandeep29175@gmail.com>
2024-10-11 00:19:45 +0530

    sdd
    ssss

---
+++ test.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sandtest.sandtest
+{
+    public class test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
